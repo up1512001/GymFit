47659bba7a7932cebba8fe377b780ac34308b6f7
up1512001 <utsav.patel1215@outlook.com>
2022-06-13 14:57:29 +0530

    HeroBanner completed

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchExercises = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchExercises
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React , { useState } from 'react'
+
+import {Box} from '@mui/material';
+
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
   )
 }
 
