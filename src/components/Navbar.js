09e2f4fae572f2966a108a9d4c825a20b2c69783
up1512001 <utsav.patel1215@outlook.com>
2022-06-13 13:57:22 +0530

    updated upto basic nessary components

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Link} from 'react-router-dom';
+
+import {Stact} from '@mui/material';
+
+import Logo from '../assets/images/Logo.png'
+
+const Navbar = () => {
+  return (
+    <Stact>
+      
+    </Stact>
+  )
+}
+
+export default Navbar
