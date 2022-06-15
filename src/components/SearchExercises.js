91703b450138b0a9f8a040661c37cda9a78e48e2
up1512001 <utsav.patel1215@outlook.com>
2022-06-15 10:04:01 +0530

    search field is added

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,84 @@
-import React from 'react'
+import React ,{ useEffect , useState} from 'react';
+
+import {Box, Button, Stack ,TextField,Typography} from '@mui/material';
+
+
 
 const SearchExercises = () => {
+
+  const [search,setSearch] = useState('');
+
+  const handleSearch = async () => {
+    if(search){
+      // const exercisesData = await fetchData();
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
+    <Stack alignItems="center" mt = "37px"  justifyContent="center" p="20px">
+      <Typography fontWeight={700} 
+        sx = {{
+          fontSize : {
+            lg : '44px',
+            xs : '30px',
+          }
+        }}
+        mb = "50px"
+        textAlign="center"
+      >
+        Awesome Exercises <br />
+        You Should Know 
+      </Typography>
+
+      <Box position="relative" mb="72px" >
+        <TextField 
+          sx={{
+            input : {
+              fontWeight : "700",
+              border : 'none',
+              borderRadius : '4px',
+            },
+            width : {
+              lg : '800px',
+              xs : '350px',
+            },
+            backGroundColor : "#fff",
+            borderRadius : '40px',
+
+          }}
+          height="76px"
+          value={search}
+          onChange={(e)=>setSearch(e.target.value).toLowerCase()}
+          placeholder= "Search Exercises"
+          type="text"
+
+        />
+
+        <Button className='search-btn' 
+          sx = {{
+            bgcolor : '#ff2625',
+            color : '#fff',
+            textTransform : 'none',
+            width : {
+              lg : '175px',
+              xs : '80px',
+            },
+            fontSize : {
+              lg : '20px',
+              xs : '14px',
+            },
+            height : '56px',
+            position : 'absolute',
+            right : '0',
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+
+      </Box>
+
+    </Stack>
   )
 }
 
---
+++ src/utils/fetchData.js
@@ -0,0 +1,5 @@
+export const fetchData = async (url,options) =>{
+    const response = await fetch(url,options);
+    const data = await response.json();
+    return data;
+}
