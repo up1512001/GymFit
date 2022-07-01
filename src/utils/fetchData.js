84b700c0eb75f9c35c54643b48f70a8ca8764831
utsav <75293077+up1512001@users.noreply.github.com>
2022-07-01 13:29:21 +0530

    api key updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -4,14 +4,16 @@ export const exerciseOptions = {
     headers: {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
         // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_KEY,
-        'X-RapidAPI-Key': 'ea80ced351msh4fa1a6600694a89p11b25ejsncf94b3c37909',
+        // 'X-RapidAPI-Key': 'ea80ced351msh4fa1a6600694a89p11b25ejsncf94b3c37909',
+        'X-RapidAPI-Key' : 'ff823cac37mshe6c04d4c310c253p12a129jsn52d1f230a67d',
     },
 };
 
 export const youtubeOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'ea80ced351msh4fa1a6600694a89p11b25ejsncf94b3c37909',
+        // 'X-RapidAPI-Key': 'ea80ced351msh4fa1a6600694a89p11b25ejsncf94b3c37909',
+        'X-RapidAPI-Key': 'ff823cac37mshe6c04d4c310c253p12a129jsn52d1f230a67d',
         'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
 };
